b291f14892e0e7cf067fc543bc73c3ec15daaf11
Akshaya-bit <akshaya2001@hotmail.com>
2021-01-24 19:45:02 +0100

    hhu

--- app/src/main/java/com/example/choose/Quiz.kt
+++ app/src/main/java/com/example/choose/Quiz.kt
@@ -2,16 +2,10 @@ package com.example.choose
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
-
-
+class Quiz : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
-}
+        setContentView(R.layout.activity_quiz)
+    }
 }
